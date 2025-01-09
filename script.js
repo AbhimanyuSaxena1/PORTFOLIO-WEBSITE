f889b27d7b2efa02b10cca6560388f07e3ca7bb4
abhimanyusaxena1 <abhisaxena5551@gmail.com>
2025-01-10 05:19:06 +0530

    added fonts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-// Register ScrollTrigger plugin
+function LocoScroll(){
+    // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
 // Initialize LocomotiveScroll
@@ -24,25 +25,31 @@ ScrollTrigger.scrollerProxy("#container", {
 scroll.on('scroll', ScrollTrigger.update);
 ScrollTrigger.addEventListener('refresh', () => scroll.update());
 ScrollTrigger.refresh();
+}
 
-gsap.to("#bottom h1",{
-    x:"-360%",
-    scrollTrigger:{
-        trigger:"#bottom",
-        scroller:"#container",
-        scrub:2,
-        pin:true,
-        start:"top 0",
-        end:"top -100%"
-    }
-
-})
+function bottomH1Animation(){
+    gsap.to("#bottom h1",{
+        x:"-350%",
+        scrollTrigger:{
+            trigger:"#bottom",
+            scroller:"#container",
+            scrub:2,
+            pin:true,
+            start:"top 0",
+            end:"top -100%"
+        }
+    
+    })
+}
 
 var tl = gsap.timeline();
-tl.from("nav",{
+function navAnimation(){
+    tl.from("nav",{
     duration:1
 })
-gsap.from("#nav1 h2",{
+
+}
+tl.from("#nav1 h2",{
     x:-50,
     duration: .8,
     // stagger : 1,
@@ -72,4 +79,35 @@ tl.from(".Rotation",{
     rotate : 45,
     opacity:0,
     // pin:true
+})
+
+
+LocoScroll()
+bottomH1Animation()
+function Splitter(){
+    var Allh1 = document.querySelectorAll("#about h1")
+    Allh1.forEach(function(item){
+        var selection = item.textContent;
+        var Splitter = selection.split("");
+        var clutter = "";
+        Splitter.forEach(function(e){
+            clutter += `<span>${e}</span>`
+        })
+        item.innerHTML = clutter ;
+    })
+}
+
+Splitter()
+gsap.to("#about h1 span",{
+    color: "whitesmoke",
+    webkitTextStroke:".8px black",
+    stagger : 0.2,
+    scrollTrigger:{
+        trigger: "#about h1",
+        scroller: "#container",
+        scrub:true,
+        start:"top 70%",
+        end:"top 10%",
+        // markers: true
+    }
 })

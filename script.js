f804fa8253439836d32df9e6e37e6c4929b18fd8
abhimanyusaxena1 <abhisaxena5551@gmail.com>
2025-01-11 05:31:40 +0530

    bottom animation made responsive

--- script.js
+++ script.js
@@ -27,87 +27,180 @@ ScrollTrigger.addEventListener('refresh', () => scroll.update());
 ScrollTrigger.refresh();
 }
 
-function bottomH1Animation(){
-    gsap.to("#bottom h1",{
-        x:"-350%",
-        scrollTrigger:{
-            trigger:"#bottom",
-            scroller:"#container",
-            scrub:2,
-            pin:true,
-            start:"top 0",
-            end:"top -100%"
-        }
-    
-    })
-}
-
 var tl = gsap.timeline();
-function navAnimation(){
-    tl.from("nav",{
-    duration:1
-})
-
-}
-tl.from("#nav1 h2",{
-    x:-50,
-    duration: .8,
-    // stagger : 1,
-    opacity : 0
-    
-    // delay : .3
-})
-gsap.from("#nav2 h2",{
-    y:-100,
-    duration: .6,
-    stagger : .3,
-    opacity : 0,
-    scale : 1.5,
-    delay : 0
-
-})
-tl.from("#text h1",{
-    x:-2000,
-    opacity:0,
-    duration:2,
-    stagger:2
-})
-tl.from(".Rotation",{
-    x:50,
-    duration:1,
-    stagger:1,
-    rotate : 45,
-    opacity:0,
-    // pin:true
-})
-
-
-LocoScroll()
-bottomH1Animation()
-function Splitter(){
-    var Allh1 = document.querySelectorAll("#about h1")
-    Allh1.forEach(function(item){
-        var selection = item.textContent;
-        var Splitter = selection.split("");
-        var clutter = "";
-        Splitter.forEach(function(e){
-            clutter += `<span>${e}</span>`
+var mm = gsap.matchMedia();
+mm.add("(min-width:601px)",()=>{
+    //DESKTOP
+    function bottomH1Animation(){
+        gsap.to("#bottom h1",{
+            x:"-350%",
+            scrollTrigger:{
+                trigger:"#bottom",
+                scroller:"#container",
+                scrub:2,
+                pin:true,
+                start:"top 0",
+                end:"top -100%"
+            }
+        
         })
-        item.innerHTML = clutter ;
-    })
-}
-
-Splitter()
-gsap.to("#about h1 span",{
-    color: "whitesmoke",
-    // webkitTextStroke:".8px red",
-    stagger : 0.2,
-    scrollTrigger:{
-        trigger: "#about h1",
-        scroller: "#container",
-        scrub:true,
-        start:"top 70%",
-        end:"top 10%",
-        // markers: true
     }
+    
+    function navAnimation(){
+        tl.from("nav",{
+        duration:.6
+    })
+    
+    }
+    tl.from("#nav1 h2",{
+        x:-50,
+        duration: 1,
+        stagger : .2,
+        opacity : 0
+        
+        // delay : .3
+    })
+    tl.from("#nav2 h2",{
+        y:-100,
+        duration: .6,
+        stagger : .3,
+        opacity : 0,
+        scale : 1.5,
+        delay : 0
+    
+    })
+    tl.from("#text h1",{
+        x:-2000,
+        opacity:0,
+        duration:2,
+        stagger:2
+    })
+    tl.from(".Rotation",{
+        x:50,
+        duration:1,
+        stagger:1,
+        rotate : 45,
+        opacity:0,
+        // pin:true
+    })
+    
+    
+    LocoScroll()
+    bottomH1Animation()
+    function Splitter(){
+        var Allh1 = document.querySelectorAll("#about h1")
+        Allh1.forEach(function(item){
+            var selection = item.textContent;
+            var Splitter = selection.split("");
+            var clutter = "";
+            Splitter.forEach(function(e){
+                clutter += `<span>${e}</span>`
+            })
+            item.innerHTML = clutter ;
+        })
+    }
+    
+    Splitter()
+    gsap.to("#about h1 span",{
+        color: "whitesmoke",
+        // webkitTextStroke:".8px red",
+        stagger : 0.2,
+        scrollTrigger:{
+            trigger: "#about h1",
+            scroller: "#container",
+            scrub:true,
+            start:"top 70%",
+            end:"top 10%",
+            // markers: true
+        }
+    })
+})
+
+mm.add("(max-width:600px)",()=>{
+    //MOBILE
+    
+    
+    function navAnimation(){
+        tl.from("nav",{
+        duration:2
+    })
+    
+    }
+    tl.from("#nav1 h2",{
+        x:-70,
+        duration: 1,
+        stagger:.5
+    })
+    tl.from("#nav2 h2",{
+        y:-100,
+        duration: .6,
+        stagger : -.4,
+    })
+    tl.from("#bottom",{
+        y: "110%",
+        scale:1.4,
+        opacity:0,
+        duration:1.5
+    })
+    function bottomH1Animation(){
+        gsap.to("#bottom h1",{
+            x:"-1192%",
+            scale:2.2,
+            scrollTrigger:{
+                trigger:"#bottom",
+                scroller:"#container",
+                scrub:5,
+                pin:true,
+                start:"top 40%",
+                end:"top -115%"
+            }
+        
+        })
+    }
+    tl.from("#text h1",{
+        x:-1000,
+        scale:3,
+        duration:1.8,
+        stagger:1.2,
+    })
+    tl.from(".Rotation",{
+        x:50,
+        scale:2,
+        duration:1,
+        stagger:1,
+        rotate : 45,
+        opacity:0,
+        // pin:true
+    })
+    
+    
+    LocoScroll()
+    bottomH1Animation()
+    function Splitter(){
+        var Allh1 = document.querySelectorAll("#about h1")
+        Allh1.forEach(function(item){
+            var selection = item.textContent;
+            var Splitter = selection.split("");
+            var clutter = "";
+            Splitter.forEach(function(e){
+                clutter += `<span>${e}</span>`
+            })
+            item.innerHTML = clutter ;
+        })
+    }
+    
+    Splitter()
+    gsap.to("#about h1 span",{
+        color: "whitesmoke",
+        // webkitTextStroke:".8px red",
+        stagger : 0.2,
+        scrollTrigger:{
+            trigger: "#about h1",
+            scroller: "#container",
+            scrub:true,
+            start:"top 70%",
+            end:"top 10%",
+            // markers: true
+        }
+    })
 })
